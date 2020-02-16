64592a5561c428d6bebbbf26beaf16c79eb4fbef
Kenneth <kenbo736@student.liu.se>
2020-02-16 17:19:26 +0100

    added files

---
+++ Lab_2/lista.h
@@ -0,0 +1,20 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <iostream>
+
+class Sorted_List{
+    public:
+        Sorted_List()
+        {}
+    private:
+        struct Node 
+        {
+            int x;
+            Node* next;
+            Node* prev;
+        }
+        
+}
+
+#endif
---
+++ Lab_2/lista_test.cc
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "catch.hpp"
+#include "Time.h"
+
+//using namespace std;
+// här lägger ni era testfall. 
+// Jobba enligt TDD; 
+//  1. Lägg till testfall
+//  2. Testa
+//  3. Lägg till (minsta möjliga) implementation
+//  4. Testa alla testfall
+//  5. Refaktorera (skriv om) så att allt ser bra ut
+
+
+TEST_CASE ("Default constructor")
+{
+    Time t;
+
+    CHECK(t.hour() == 0);
+    CHECK(t.minute() == 0);
+    CHECK(t.second() == 0);
+}
+
+TEST_CASE ( "Constructor with numeric arguments" )
+{
+    Time t{12,13,14};
+    CHECK(t.hour() == 12);
+    CHECK(t.minute() == 13);
+    CHECK(t.second() == 14);
+}
+
+TEST_CASE ("Constructor with faulty argument")
+{
+    CHECK_THROWS( Time{41,0,0} );
+    CHECK_THROWS( Time{0,99,0} );
+    CHECK_THROWS( Time{0,0,99} );
+}
+
+TEST_CASE ("String constructor")
+{
+    Time t{"12:23:12"};
+    CHECK(t.hour() == 12);
+    CHECK(t.minute() == 23);
+    CHECK(t.second() == 12);
+
+    SECTION ("Throws as well")
+    {
+        CHECK_THROWS( Time{"02:11:74"} );
+    }
+}
+
+
+// the following line will halt the compilation process. Move it
+// one test case at the time and then start creating your own test
+// cases
+#if 0
+
+#endif
+
