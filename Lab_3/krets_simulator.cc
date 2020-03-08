2b71f685d039060f0f22bdb51555d7161c60c059
kenbo736 <kenbo736@su15-120.ad.liu.se>
2020-03-08 16:02:02 +0100

    updated files with relevant information

--- Lab_3/krets_simulator.cc
+++ Lab_3/krets_simulator.cc
@@ -1,7 +1,27 @@
 #include <iostream>
 #include "krets_simulator.h"
 
+Component::Component()
+{
+  //behöver ingång och utgång men också namn för componeneten
+}
+
+double Component::get_current()
+{
+
+}
+
+void Component::simulate()
+{
+
+}
+
+double Component::get_voltage()
+{
+
+}
+
 void simulate()
 {
-  
+
 }
--- Lab_3/krets_simulator.h
+++ Lab_3/krets_simulator.h
@@ -7,35 +7,46 @@ class Connect
 {
 public:
   double voltage{};
+  double get_voltage();
+  void set_voltage();
+
 };
 
-class Component // består av connection
+class Component // bas klassen
 {
 public:
+  Component(); //default
+  Component(std::string name, C);
   virtual double get_current() = 0;
   virtual void simulate() = 0;
+  double get_voltage();
 
 protected:
-  virtual char a{};
-  virtual char b{};
+  virtual Connect a{};
+  virtual Connect b{};
 
 };
 
 class Battery : public Component
 {
+public:
+  void simulate();
+
 private:
+  double voltage{};
 
 };
 
-class Resistor : public Component
+/*class Resistor : public Component
 {
-
+public:
+  double get_current() override;
 };
 
 class Capacitor : public Component
 {
 
-};
+};*/
 
 void simulate();
 
